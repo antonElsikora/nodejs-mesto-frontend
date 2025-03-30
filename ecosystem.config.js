8e65298c3fd1fea1432b63a4cb66170d100ff682
antonElsikora <anton.developer@elsikora.com>
2025-03-30 05:44:18 +0300

    FIX: rebuild ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,8 +18,10 @@ module.exports = {
       ref: process.env.DEPLOY_REF,
       repo: process.env.DEPLOY_REPO,
       path: process.env.DEPLOY_PATH,
+      'pre-deploy-local':
+        `scp .env ${process.env.DEPLOY_USER}@${process.env.DEPLOY_HOST}:${process.env.DEPLOY_PATH}/current/`,
       'post-deploy':
-        'npm install && npm run build && pm2 restart ecosystem.config.js --env production',
+        'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 restart ecosystem.config.js --env production',
     },
   },
 };
