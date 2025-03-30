b05287a8ec499ac63b288ef67979e420da5dd768
antonElsikora <anton.developer@elsikora.com>
2025-03-30 05:55:39 +0300

    FIX: update deployment configuration for frontend

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -21,7 +21,7 @@ module.exports = {
       'pre-deploy-local':
         `scp .env ${process.env.DEPLOY_USER}@${process.env.DEPLOY_HOST}:${process.env.DEPLOY_PATH}/current/`,
       'post-deploy':
-        'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 restart ecosystem.config.js --env production',
+          'source ~/.nvm/nvm.sh && export NODE_OPTIONS=--openssl-legacy-provider && npm install && npm run build && pm2 restart ecosystem.config.js --env production',
     },
   },
 };
